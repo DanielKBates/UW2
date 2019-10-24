662bd26d9e1ce5683082c599072314f24c9c4d50
DanielKBates <danielbates95@gmail.com>
2019-10-24 17:05:24 -0400

    initial commit

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,34 @@
+var path = require("path");
+
+
+module.exports = function(app) {
+
+  app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname,"../public/index.html"))
+  });
+  app.get("/aboutRepoman", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/aboutRepoman.html"));
+  });
+
+  app.get("/aboutUSAWeb", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/aboutUSAWeb.html"));
+  });
+  app.get("/contactUs", function(req, res){
+    res.sendFile(path.join(__dirname,"../public/contactUs.html"))
+  });
+  app.get("/directory", function(req, res){
+    res.sendFile(path.join(__dirname,"../public/directory.html"))
+  });
+  app.get("/insurance", function(req, res){
+    res.sendFile(path.join(__dirname,"../public/insurance.html"))
+  });
+  app.get("/listingOptions", function(req, res){
+    res.sendFile(path.join(__dirname,"../public/listingOptions.html"))
+  });
+
+
+  // If no matching route is found default to home
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+};
---
+++ server.js
@@ -0,0 +1,23 @@
+
+
+var express = require("express");
+
+
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+require("./routes/htmlRoutes")(app);
+// require("./routes/apiRoutes")(app);
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
+module.exports = app;
