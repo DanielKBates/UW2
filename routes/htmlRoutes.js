b32b5a583b98afdf993929e13a168c100c66b369
Joey <the.moose2023@gmail.com>
2019-10-29 12:58:41 -0400

    comment updated insurance, listingOption pages

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,36 +3,38 @@ var path = require("path");
 
 module.exports = function(app) {
 
-  app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname,"../public/index.html"))
-  });
-  app.get("/aboutRepoman", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/aboutRepoman.html"));
-  });
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"))
+    });
+    app.get("/aboutRepoman", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/aboutRepoman.html"));
+    });
 
-  app.get("/aboutUSAWeb", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/aboutUSAWeb.html"));
-  });
-  app.get("/contactUs", function(req, res){
-    res.sendFile(path.join(__dirname,"../public/contactUs.html"))
-  });
-  app.get("/directory", function(req, res){
-    res.sendFile(path.join(__dirname,"../public/directory.html"))
-  });
-  app.get("/insurance", function(req, res){
-    res.sendFile(path.join(__dirname,"../public/insurance.html"))
-  });
-  app.get("/listingOptions", function(req, res){
-    res.sendFile(path.join(__dirname,"../public/listingOptions.html"))
-  });
-  app.get("/sitePolicy", function(req, res){
-    res.sendFile(path.join(__dirname,"../public/sitePolicy.html"))
-  });
- 
+    app.get("/aboutUSAWeb", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/aboutUSAWeb.html"));
+    });
+    app.get("/contactUs", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/contactUs.html"))
+    });
+    app.get("/directory", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/directory.html"))
+    });
+    app.get("/insurance", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/insurance.html"))
+    });
+    app.get("/listingOptions", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/listingOptions.html"))
+    });
+    app.get("/sitePolicy", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/sitePolicy.html"))
+    });
+    app.get("/refund", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/refund.html"))
+    });
 
 
-  // If no matching route is found default to home
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
+    // If no matching route is found default to home
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
 };
